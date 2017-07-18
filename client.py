14e0fad8801cf37f2a590d6ab634e6a5e60a11a0
Tom Robertson <tom_r3@hotmail.com>
2017-07-18 14:36:57 -0400

    Added Multithreading and OS Assigned Ports

--- client.py
+++ client.py
@@ -1,24 +1,32 @@
-import socket
+import socket, sys
+
+#download: client <host> <port> G<key> <file name> <recv size>
+#upload: client <host> <port> P<key> <file name> <send size> <wait time>
 
 s = socket.socket()
-host = socket.gethostname()
-port = 60000
+host = sys.argv[1] #socket.gethostname()
+port = int(sys.argv[2])
+
+BUFFER_SIZE = 1024
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+print 'host: ' + host + ', port: ' + str(port)
 
 s.connect((host, port))
-s.send("Hello server!")
-
-with open('received_file', 'wb') as f:
+with open(sys.argv[3], 'wb') as f:
     print 'file opened'
     while True:
-        print('receiving data...')
-        data = s.recv(1024)
+        #print('receiving data...')
+        data = s.recv(BUFFER_SIZE)
         print('data=%s', (data))
         if not data:
+            f.close()
+            print 'file close()'
             break
-            
+        # write data to a file
         f.write(data)
 
-f.close()
 print('Successfully get the file')
 s.close()
 print('connection closed')
--- server.py
+++ server.py
@@ -1,12 +1,56 @@
 import socket
+from threading import Thread
+from SocketServer import ThreadingMixIn
 
-port = 60000
-s = socket.socket()
-host = socket.gethostname()
-s.bind((host, port))
-s.listen(5)
+BUFFER_SIZE = 1024
 
-print 'Server listening....'
+class ClientThread(Thread):
+
+    def __init__(self,ip,port,sock):
+        Thread.__init__(self)
+        self.ip = ip
+        self.port = port
+        self.sock = sock
+        print " New thread started for "+ip+":"+str(port)
+
+    def run(self):
+        filename='mytext.txt'
+        f = open(filename,'rb')
+        while True:
+            l = f.read(BUFFER_SIZE)
+            while (l):
+                self.sock.send(l)
+                #print('Sent ',repr(l))
+                l = f.read(BUFFER_SIZE)
+            if not l:
+                f.close()
+                self.sock.close()
+                break
+
+# create connection
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) # tell the kernel to reuse a local socket in TIME_WAIT state, without waiting for its natural timeout to expire
+s.bind(("",0))
+port = s.getsockname()[1]
+
+threads = []
+
+# listen for incoming connections
+while True:
+    s.listen(5)
+    print "Waiting for incoming connections on port " + str(port) + " and host " + socket.gethostname()
+    (conn, (ip,port)) = s.accept()
+    print 'Got connection from ', (ip,port)
+    newthread = ClientThread(ip,port,conn)
+    newthread.start()
+    threads.append(newthread)
+
+for t in threads:
+    t.join()
+
+# write port number to file for client
+with open('port', 'wb') as p: 
+    p.write(str(port))
 
 while True:
     conn, addr = s.accept()
